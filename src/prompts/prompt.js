58da57e56b08734d34d0ffeea622105fc8a601ee
jywarren <jeff@unterbahn.com>
2016-08-25 19:06:50 -0400

    xhrOptions options param to pass to bureaucracy

--- src/prompts/prompt.js
+++ src/prompts/prompt.js
@@ -112,6 +112,7 @@ function prompt (options, done) {
       method: upload.method,
       formData: upload.formData,
       fieldKey: upload.fieldKey,
+      xhrOptions: upload.xhrOptions,
       endpoint: upload.url,
       validate: 'image'
     });
