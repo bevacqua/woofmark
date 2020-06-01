3d99ce1a162e0544afc926cf741c6cdf399f797d
NitinBhasneria <nitinbhasnerai360@gmail.com>
2020-06-01 14:27:39 +0530

    Drag and drop css

--- src/uploads.js
+++ src/uploads.js
@@ -5,25 +5,35 @@ var classes = require('./classes');
 var dragClass = 'wk-dragging';
 var dragClassSpecific = 'wk-container-dragging';
 var root = document.documentElement;
+var dragginCss = 0;
 
 function uploads (container, droparea, editor, options, remove) {
   var op = remove ? 'remove' : 'add';
-  crossvent[op](root, 'dragenter', dragging);
-  crossvent[op](root, 'dragend', dragstop);
-  crossvent[op](root, 'mouseout', dragstop);
+  crossvent[op](root, 'dragend', dragstopforce);
+  crossvent[op](root, 'mouseout', dragstopforce);
   crossvent[op](container, 'dragover', handleDragOver, false);
+  crossvent[op](container, 'dragenter', dragging, false);
+  crossvent[op](container, 'dragleave', dragstop, false);
   crossvent[op](droparea, 'drop', handleFileSelect, false);
 
   function dragging () {
+    dragginCss++;
     classes.add(droparea, dragClass);
     classes.add(droparea, dragClassSpecific);
   }
   function dragstop () {
+    dragginCss--;
+    if(dragginCss === 0){
+      dragstopper(droparea);
+    }
+  }
+  function dragstopforce () {
     dragstopper(droparea);
   }
   function handleDragOver (e) {
     stop(e);
-    dragging();
+    classes.add(droparea, dragClass);
+    classes.add(droparea, dragClassSpecific);
     e.dataTransfer.dropEffect = 'copy';
   }
   function handleFileSelect (e) {
